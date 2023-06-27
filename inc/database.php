433ca0a3ea7312dd9a652b2837db1eaeb1a85559
JAbinSin <johnalvincruz93@gmail.com>
2023-06-27 19:54:49 +0800

    ADD: Modals for DB errors

--- inc/database.php
+++ inc/database.php
@@ -11,6 +11,11 @@
 
     // Check connection
     if ($connection->connect_errno) {
+        include("../modals/modal.php");
+        ?>
+        <script>document.getElementById("myModalOutput").innerHTML = "Error occured. Please try again later. <br><?php echo $connection->error; ?>"</script>
+        <script>myModal.show()</script>
+        <?php
         echo "Failed to connect to MySQL: " . $connection->connect_error;
         exit();
     }
